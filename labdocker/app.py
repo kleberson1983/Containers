05c18f7777510d0fb2d179fd166bc16e2545c809
kleberson1983 <kleberson.brandao@hotmail.com>
2021-10-11 20:52:17 -0300

    Atividade final
    Desafio completo

--- labdocker/app.py
+++ labdocker/app.py
@@ -4,7 +4,7 @@ from redis import Redis, RedisError
 import os
 
 # Connect to Redis
-redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
+redis = Redis(host=os.getenv("REDIS_HOST"), db=0, socket_connect_timeout=2, socket_timeout=2)
 
 # Objeto da Classe Flask que vamos usar para configurar e executar a aplicação
 app = Flask(__name__)
@@ -21,7 +21,7 @@ def hello():
 
     html = "<h3>{message}</h3>" \
            "<b>Visits:</b> {visits}"
-    return html.format(message=os.getenv("MESSAGE", "Hello world"), visits=visits)
+    return html.format(message=os.getenv("MESSAGE", "Hello World "), visits=visits)
 
 # Garantindo que o módulo não será executado se ele for importado por outro módulo.
 if __name__ == "__main__":
